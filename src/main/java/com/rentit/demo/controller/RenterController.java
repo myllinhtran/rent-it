47dd1a1702118941a8966cc16826d5b559142563
myllinhtran <mylinh3591@gmail.com>
2020-08-26 12:25:48 +0200

    Update renter

--- src/main/java/com/rentit/demo/controller/RenterController.java
+++ src/main/java/com/rentit/demo/controller/RenterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.MessageFormat;
 import java.util.Optional;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping(path = "/api/v1")
 public class RenterController {
