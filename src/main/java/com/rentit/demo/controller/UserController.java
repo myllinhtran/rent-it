f8e024e79170f4baf192a9be63e118ba2879a804
myllinhtran <mylinh3591@gmail.com>
2020-10-19 18:44:17 +0200

    Update UserController.java

--- src/main/java/com/rentit/demo/controller/UserController.java
+++ src/main/java/com/rentit/demo/controller/UserController.java
@@ -3,11 +3,11 @@ package com.rentit.demo.controller;
 import com.rentit.demo.model.User;
 import com.rentit.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
