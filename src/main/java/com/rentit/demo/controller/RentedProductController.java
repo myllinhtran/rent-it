0e27d0f7295b126d496911dac4643afb8463b5bd
myllinhtran <mylinh3591@gmail.com>
2020-10-19 17:16:48 +0200

    Create renteRentedProductController.java

---
+++ src/main/java/com/rentit/demo/controller/RentedProductController.java
@@ -0,0 +1,21 @@
+package com.rentit.demo.controller;
+
+import com.rentit.demo.model.RentedProduct;
+import com.rentit.demo.repository.RentedProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
+public class RentedProductController {
+
+    @Autowired
+    private RentedProductRepository repository;
+
+    @GetMapping(path = "/rentedProducts")
+    public @ResponseBody
+    Iterable<RentedProduct> getAllRentedProducts() {
+        return repository.findAll();
+    }
+}
