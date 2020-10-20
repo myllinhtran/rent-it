9a11a4b49d7c1f8aa8793d16a23129cb1f82ce6f
myllinhtran <mylinh3591@gmail.com>
2020-10-20 12:00:59 +0200

    Add method of passing parameter to User

--- src/main/webapp/reactjs/src/components/MyProfile.js
+++ src/main/webapp/reactjs/src/components/MyProfile.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {Tabs, Tab} from 'react-bootstrap';
-import Account from "./Account";
+import {Tabs, Tab} from "react-bootstrap";
 import MyProducts from "./MyProducts";
+import axios from "axios";
+import User from "./User";
 
 class MyProfile extends Component {
 
@@ -14,31 +15,51 @@ class MyProfile extends Component {
         this.handleSelect = this.handleSelect.bind(this);
     }
 
+    componentDidMount() {
+        const userId = this.props.match.params.id;
+        if (userId) {
+            this.findAccountById(userId);
+        }
+    };
+
+    findAccountById = (userId) => {
+        axios.get("https://safe-sierra-04090.herokuapp.com/api/v1/users/" + userId)
+            .then(response => {
+                if (response.data != null) {
+                    this.setState({
+                        id: response.data.id,
+                        firstName: response.data.firstName,
+                        lastName: response.data.lastName,
+                        mobile: response.data.mobile,
+                        email: response.data.email
+                    })
+                }
+            })
+    };
+
     handleSelect(selectedTab) {
         this.setState({
-            activeTab: selectedTab
+            activeTab: selectedTab,
         });
-    }
+    };
 
     render() {
+
+        const {id, firstName, lastName, mobile, email} = this.state;
+
         return (
             <div className={"card-body"}>
-                <Tabs defaultActiveKey={"account"} onSelect={this.handleSelect}>
-                    <Tab eventKey={"account"} title={"Account"}>
-                        <div>
-                            <p><Account/></p>
-                        </div>
+                <Tabs defaultActiveKey={"user"} onSelect={this.handleSelect}>
+                    <Tab eventKey={"user"} title={"User"}>
+                        <User
+                            id={id}
+                            firstName={firstName}
+                            lastName={lastName}
+                            mobile={mobile}
+                            email={email}
+                        />
                     </Tab>
-                    <Tab eventKey={"rentedProducts"} title={"Rented Products"}>
-                        <div>
-                            <p><MyProducts/></p>
-                        </div>
-                    </Tab>
-                    <Tab eventKey={"rentingProducts"} title={"Renting Products"}>
-                        <div>
-                            <p><MyProducts/></p>
-                        </div>
-                    </Tab>
+                    <Tab eventKey={"rentedProducts"} title={"Rented Products"}><MyProducts/></Tab>
                 </Tabs>
             </div>
         );
