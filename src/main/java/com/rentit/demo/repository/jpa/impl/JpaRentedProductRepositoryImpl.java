8a21cb216240441d33dd03001d68050bb7738ccf
myllinhtran <mylinh3591@gmail.com>
2020-10-24 01:44:30 +0200

    Fix bugs

--- src/main/java/com/rentit/demo/repository/jpa/impl/JpaRentedProductRepositoryImpl.java
+++ src/main/java/com/rentit/demo/repository/jpa/impl/JpaRentedProductRepositoryImpl.java
@@ -26,7 +26,7 @@ public class JpaRentedProductRepositoryImpl implements JpaRentedProductRepositor
     public Collection<RentedProduct> getProductByAccount() {
         //SELECT rentedProduct FROM RentedProduct rentedProduct WHERE rentedProduct.renterId.id= :id
         Query query = this.entityManager.createQuery(
-                "SELECT categoryId FROM RentedProduct rentedProduct left join fetch rentedProduct.categoryId categoryId");
+                "SELECT rentedProduct, category FROM RentedProduct rentedProduct join rentedProduct.categoryId category");
         return query.getResultList();
     }
 }
