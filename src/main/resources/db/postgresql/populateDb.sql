f7872ae2097cb506970152fe9e58a330b804453f
myllinhtran <mylinh3591@gmail.com>
2020-10-26 16:17:33 +0100

    Add data

---
+++ src/main/resources/db/postgresql/populateDb.sql
@@ -0,0 +1,11 @@
+# insert into rentit.accounts values (default, 'Eduard', 'Feliu Baena', 'feliu06@gmail.com', '634879172');
+# insert into rentit.accounts values (default, 'Brian', 'Rivers', 'brianreviers@gmail.com', '673849718');
+# insert into rentit.accounts values (default, 'Micheal', 'Hoffman', 'michealhoffman@gmail.com', '672893746');
+# insert into rentit.accounts values (default, 'Lea', 'Reilly', 'leareilly@gmail.com', '672839456');
+# insert into rentit.accounts values (default, 'Reed', 'Dean', 'reeddean@gmail.com', '672536490');
+# insert into rentit.accounts values (default, 'Bob', 'Cat', 'bobcat@gmail.com', '654839102');
+
+
+# insert into rentit.rented_products values (default, 3, 7, 2, 5, 'Ha Noi', '2020-10-26', '2020-10-30');
+# insert into rentit.rented_products values (default, 2, 8, 3, 5, 'Ha Noi', '2020-10-26', '2020-10-30');
+# insert into rentit.rented_products values (default, 3, 5, 4, 6, 'Quang Binh', '2020-10-24', '2020-10-27');
