11df4899472e2cca06deb2500b08fa8103eb03b8
myllinhtran <mylinh3591@gmail.com>
2020-10-27 18:51:12 +0100

    Fix bugs

--- src/main/java/com/rentit/demo/rest/CategoryController.java
+++ src/main/java/com/rentit/demo/rest/CategoryController.java
@@ -26,8 +26,8 @@ public class CategoryController {
     }
 
     @GetMapping(path = "/categories/category")
-    public @ResponseBody String getCategoryByName(@RequestParam String category) {
-        return "Category: " + category;
+    public @ResponseBody String getCategoryByName(@RequestParam String name) {
+        return "Category: " + name;
     }
 
     @PostMapping(path = "/categories")
