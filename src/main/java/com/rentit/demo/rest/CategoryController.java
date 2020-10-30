c1d5e5ee03c7f3bebce8adb1a4559207c8956d03
myllinhtran <mylinh3591@gmail.com>
2020-10-30 16:22:14 +0100

    Update project

--- src/main/java/com/rentit/demo/rest/CategoryController.java
+++ src/main/java/com/rentit/demo/rest/CategoryController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.Collection;
+import java.util.Optional;
 
 
 @RestController
@@ -27,5 +29,16 @@ public class CategoryController {
     @GetMapping(path = "/categories")
     public ResponseEntity<Collection<Category>> getAll() {
         return ResponseEntity.ok(categoryRepository.findAll());
+    }
+
+    @GetMapping(path = "/categories/{id}")
+    public ResponseEntity<Category> getById(@PathVariable("id") int id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        else {
+            return ResponseEntity.ok(optionalCategory.get());
+        }
     }
 }
