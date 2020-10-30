38c1ae454e24a7fcad7d708b2ec583f5c5c3175a
myllinhtran <mylinh3591@gmail.com>
2020-10-30 16:17:22 +0100

    Update project

--- src/main/java/com/rentit/demo/rest/ProductController.java
+++ src/main/java/com/rentit/demo/rest/ProductController.java
@@ -1,8 +1,12 @@
 package com.rentit.demo.rest;
 
+import com.rentit.demo.model.Product;
 import com.rentit.demo.repository.CategoryRepository;
 import com.rentit.demo.repository.ProductRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 
 @RestController
@@ -12,9 +16,14 @@ public class ProductController {
 
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
-    
+
     public ProductController(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping(path = "/products")
+    public ResponseEntity<Collection<Product>> getAll() {
+        return ResponseEntity.ok(productRepository.findAll());
     }
 }
