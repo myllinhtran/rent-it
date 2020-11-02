bdfb117384eee90e57439db75323f39d315888a9
myllinhtran <mylinh3591@gmail.com>
2020-11-02 17:28:42 +0100

    Test

--- src/main/java/com/rentit/demo/repository/CategoryRepository.java
+++ src/main/java/com/rentit/demo/repository/CategoryRepository.java
@@ -4,5 +4,5 @@ import com.rentit.demo.model.Category;
 import com.rentit.demo.repository.jpa.JpaCategoryRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CategoryRepository extends JpaCategoryRepository, CrudRepository<Category, Integer>, CategoryRepositoryOverride {
+public interface CategoryRepository extends JpaCategoryRepository, CrudRepository<Category, Integer> {
 }
