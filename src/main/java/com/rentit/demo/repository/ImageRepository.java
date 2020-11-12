881401bc3f42196f8094ae9fe3321d0a421110f5
myllinhtran <mylinh3591@gmail.com>
2020-11-12 19:17:43 +0100

    Fix bugs

--- src/main/java/com/rentit/demo/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentit.demo.repository;
-
-import com.rentit.demo.model.Image;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ImageRepository extends CrudRepository<Image, Integer> {
-}
